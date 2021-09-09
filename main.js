f191edc0bd8b0ec3cd3ea010334d5eca703ab3bb
DevankLikesCodingStuff <77108500+DevankLikesCodingStuff@users.noreply.github.com>
2021-09-09 20:26:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,28 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+  //classifier will load the mobileNet model
 }
-
-
+function draw(){
+  image(video, 0, 0, 300, 300);
+  //Image function will put the webcam on the canvas
+  classifier.classify(video, gotResult);
+  //Classify will do the comparision
+}
+function modelLoaded()
+{
+  console.log('Model Loaded!');
+}
+function gotResult(error, results)
+{
+  if(error){
+    console.error(error);
+  }
+  else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
 
